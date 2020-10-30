a4935814443e6b41aa92c29b0daa5840afa18261
caesaraugustus1738 <71141254+caesaraugustus1738@users.noreply.github.com>
2020-10-30 13:11:38 +0000

    Tidied code. Now time for hooking up front and back end.

--- gui.py
+++ gui.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title('GT Secure Archives 1.0')
+root.title('GT Secure Archiver 1.0')
 
 HEIGHT = 300
 WIDTH = 700
+FONT = 'helvetica'
 PINK = '#ead0d6'
+WHITE = 'white'
+ACCENT_1 = '#9bd2c6'
+ENTRY_TEXT_BG = WHITE
 
 layout_preview = 0
 
@@ -16,14 +20,14 @@ else:
 	interaction_colour = PINK
 	line_colour = PINK
 
-secure_colour = '#9bd2c6'
 
-frame = tk.Frame(root, height=HEIGHT, width=WIDTH,\
-	bg=PINK, bd=4)
+frame = tk.Frame(root, width=WIDTH, height=HEIGHT, bg=PINK, bd=4)
 frame.pack()
 
-interaction = tk.Frame(frame, height=200, width=WIDTH, bg=interaction_colour, bd=4)
+
+interaction = tk.Frame(frame, width=WIDTH, height=200, bg=interaction_colour, bd=4)
 interaction.place(height=200, relwidth=1, relx=.005, y=100)
+
 
 lines = {}
 for i in range(6):
@@ -32,48 +36,37 @@ for i in range(6):
 	y = 0.15*i
 	lines['l' + str(i)].place(height=20, width=670, relx=0, rely=y)
 
-print(lines)
 
 path_to_archive = tk.Label(lines['l1'], text='Path to be archived', bg=PINK)
 path_to_archive.place(x=1)
 
-archive_entry = tk.Entry(lines['l1'], bg='white',\
-	font='helvetica',
-	bd=0,
-	highlightcolor='white',
-	width=65
-	)
+
+archive_entry = tk.Entry(lines['l1'], bg=ENTRY_TEXT_BG, font=FONT, bd=0, highlightcolor=WHITE, width=65)
 archive_entry.place(x=145)
+
 
 dest = tk.Label(lines['l2'], text='Destination', bg=PINK)
 dest.place(x=1)
 
 
-dest_entry = tk.Entry(lines['l2'], bg='white',\
-	font='helvetica',
-	bd=0,
-	highlightcolor='white',
-	width=65
-	)
-
+dest_entry = tk.Entry(lines['l2'], bg='white', font=FONT, bd=0, highlightcolor=WHITE, width=65)
 dest_entry.place(x=145)
 
-# key_label = tk.Label(lines['l3'], text='Key',bg='#e2bec7')
-# key_label.place(x=145, relheight=1, width=40)
 
 key_gen_label = tk.Label(lines['l3'], text='[copy and paste the key from here]', bg='white')
 key_gen_label.place(x=145, relheight=1, width=570)
 
-secure_button = tk.Checkbutton(lines['l4'], text='Secure', variable=None, \
-                 onvalue=1, offvalue=0, selectcolor='red', \
-                 bg=secure_colour)
+
+secure_button = tk.Checkbutton(lines['l4'], text='Encrypt', variable=None, \
+                 onvalue=1, offvalue=0, bg=ACCENT_1)
 secure_button.place(x=145, relheight=1, width=150)
 
 
-
-archive_button = tk.Button(lines['l5'], text='Archive',\
-	activebackground='blue')
+archive_button = tk.Button(lines['l5'], text='Archive', activebackground=ACCENT_1)
 archive_button.place(x=530, relheight=1, width=150)
+
+
+root.mainloop()
 
 # dest = tk.Label(line2, text='Archive destination',\
 # 	bg=PINK)
@@ -126,4 +119,3 @@ archive_button.place(x=530, relheight=1, width=150)
 # 	relx=.1, rely=.1)
 
 
-root.mainloop()
